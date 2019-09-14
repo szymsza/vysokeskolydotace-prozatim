e46dd3747401aa17570faac6b8b9ef5e6e49cd03
Jakub Szymsza <jakub@szymsza.cz>
2019-09-14 17:47:42 +0200

    Nav logo

--- src/views/components/Navbar.vue
+++ src/views/components/Navbar.vue
@@ -6,7 +6,7 @@
     dark
   >
     <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-      <span class="hidden-sm-and-down">Kam za vědou</span>
+      <img src="@/assets/logo.png" /><span>Kam za vědou</span>
     </v-toolbar-title>
 
     <v-autocomplete
